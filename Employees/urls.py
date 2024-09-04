47a00f7f8c1d0d3154f2a026b4c4c7ae8485eff4
kashishpal6 <kashishpal386@gmail.com>
2024-09-04 13:51:40 +0530

    authentication

--- Employees/urls.py
+++ Employees/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from Employees.views import Employee,listEmployees,UpdateEmployee,DestroyEmployee,RetrieveEmployee
+from Employees.views import Employee,listEmployees,UpdateEmployee,DeleteEmployee,RetrieveEmployee
 
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('employee-list/',listEmployees.as_view(),name="list_employees"),
     path('update-employee/<int:pk>',UpdateEmployee.as_view(),name="update-employee"),
     path('retrieve-employee/<int:pk>',RetrieveEmployee.as_view(),name="retrieve-employee"),
-    path('delete-employee/<int:pk>',DestroyEmployee.as_view(),name="destroy-employee"),
+    path('delete-employee/<int:pk>',DeleteEmployee.as_view(),name="destroy-employee"),
     
 
    
