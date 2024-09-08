80567596d09481555e4c8f3f3bf8eb337466ec7b
kashishpal6 <kashishpal386@gmail.com>
2024-09-08 14:08:25 +0530

    retrieve employee

--- Employees/views.py
+++ Employees/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
 from .models import Employees
 from .serializers import employeesSerializer
-from rest_framework import generics
+from rest_framework import generics,response,status
 from rest_framework.permissions import AllowAny,IsAuthenticated
+from Project_management.models import Project_manager
+from Projects.serializers import projectsSerializer
+from Projects.models import *
+from Tasks.models import Tasks
+from Tasks.serializers import TasksSerializer
 
 class Employee(generics.CreateAPIView):
    queryset=Employees.objects.all()
@@ -27,12 +31,29 @@ class DeleteEmployee(generics.DestroyAPIView):
    permission_classes =[IsAuthenticated]
 
 class RetrieveEmployee(generics.RetrieveAPIView):
-   queryset=Employees.objects.all()
-   serializer_class=employeesSerializer 
-   permission_classes =[AllowAny]
+    serializer_class = employeesSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        emp_data = Employees.objects.get(Employee_id=self.kwargs['pk'])
+        project_managers = Project_manager.objects.filter(name__Employee_id=self.kwargs['pk'])
+        emp_serialized = employeesSerializer(emp_data).data
+        projects = [project_manager.project for project_manager in project_managers]
+        project_serialized = projectsSerializer(projects, many=True).data
+        tasks = Tasks.objects.filter(Assigned_to__name=emp_data)
+        tasks_serialized = TasksSerializer(tasks, many=True).data
+
+        data = {
+            'employee_data': emp_serialized,
+            'projects': project_serialized,
+            'tasks': tasks_serialized
+         }
+        return response.Response(data=data, status=status.HTTP_200_OK)
 
 
-    
+   
 
 
+        
 
+        
