180ff55c37e2d2bcc4bfd8c62e24a27e244f39fb
srivastavatanuj <tanuj-srivastava@outlook.com>
2024-10-01 10:38:25 +0530

    chat app

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack 
 from Chat.consumer import *
 from django.urls import path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
@@ -16,10 +17,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 application = get_asgi_application()
 
 ws_patterns = [
-    path('ws/test/',TestConsumer.as_asgi())
+    path('ws/chat/',ChatConsumer.as_asgi()),
+    path('ws/online/',ChatConsumer.as_asgi()),
+    path('ws/notify/',ChatConsumer.as_asgi()),
 ]
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
-    'websocket':URLRouter(ws_patterns)
+    'websocket':AuthMiddlewareStack(URLRouter(ws_patterns))
     
 })
--- backend/settings.py
+++ backend/settings.py
@@ -174,7 +174,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("127.0.0.1", 6379),("0.0.0.0",6379)],
         },
     },
 }
